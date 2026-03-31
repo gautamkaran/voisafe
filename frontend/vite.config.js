6524fb5e2730a36cf5c5b5bb40945e47f7337762
gautamkaran <karankumargautam.dev@gmail.com>
2026-03-31 22:09:28 +0530

    fix: update SERVER_URL to production API and configure server settings in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   define: {
-    SERVER_URL: JSON.stringify("http://localhost:3000/api"),
+    SERVER_URL: JSON.stringify("https://api.gautamkaran.cloud"),
+  },
+  server: {
+    port: 4173,
+    host: true,
+    allowedHosts: ["gautamkaran.cloud", "api.gautamkaran.cloud"],
   },
 });
