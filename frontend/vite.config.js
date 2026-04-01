27eba21329df43e69b96edd1bf150c539fc49671
gautamkaran <karankumargautam.dev@gmail.com>
2026-04-01 06:30:07 +0530

    feat: configure vite with react, tailwindcss, and server environment settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   define: {
-    // SERVER_URL: JSON.stringify("https://api.gautamkaran.cloud/api"),
-    SERVER_URL: JSON.stringify("http://localhost:3000/api"),
+    SERVER_URL: JSON.stringify("https://api.gautamkaran.cloud/api"),
+    // SERVER_URL: JSON.stringify("http://localhost:3000/api"),
   },
   server: {
     port: 4173,
